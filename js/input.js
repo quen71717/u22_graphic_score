d066e195086b7f2ebb1fda3599bb11ce2420c69a
kokoro-origami <ri_takukou@yahoo.co.jp>
2025-05-27 14:58:35 +0900

    ト音記号画像のパスを修正

--- js/input.js
+++ js/input.js
@@ -1,7 +1,9 @@
 "use strict";
 
 // ト音記号画像のパス
-const trebleClefImagePath = "../img/to-onkigou.png";
+const trebleClefImagePath = "img/to-onkigou.png";
+// Go Live用パス
+// const trebleClefImagePath = "../img/to-onkigou.png";
 
 // 上から一つ目の五線譜の一本目の線の縦軸(単位:pixel)
 const initialY = 50;
