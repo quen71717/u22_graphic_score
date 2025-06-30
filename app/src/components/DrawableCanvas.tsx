d9934fbeb2c617110b36b3e660927221032a0e0f
bisuansd28 <haihaikunndai@gmail.com>
2025-06-30 11:08:05 +0900

    konnitiha

--- app/src/components/DrawableCanvas.tsx
+++ app/src/components/DrawableCanvas.tsx
@@ -149,7 +149,7 @@ const DrawableCanvas: React.FC<DrawableCanvasProps> = ({
       setLastPosition(pos);
 
       // 座標の記録を開始
-      setDrawnCoordinates([pos]);
+      setDrawnCoordinates((prev) => [...prev, pos]);
     },
     [getMousePosition, setDrawnCoordinates]
   );
